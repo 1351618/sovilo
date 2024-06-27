10723c7f99e3e7939c3dbc4626c2a96a1f053d04
1351618 <1351618@gmail.com>
2024-06-27 14:32:19 +0300

    feat: mobile

---
+++ src/js/header.js
@@ -0,0 +1,9 @@
+const headerBurgMenuBtn = document.querySelector(".header__burg-menu-btn");
+const headerMobileMenu = document.querySelector(".header__mobile-menu");
+
+headerBurgMenuBtn.addEventListener("click", () => {
+  headerMobileMenu.classList.toggle("hide");
+});
+headerMobileMenu.addEventListener("click", () => {
+  headerMobileMenu.classList.toggle("hide");
+});
